2e340d56bf520ebece39b40c64439f977d94509c
Szczepan 'Beregond' Cieślik <szczepan.cieslik@gmail.com>
2013-01-01 13:09:05 +0100

    Modify: Improved skip list.

--- skiplist.py
+++ skiplist.py
@@ -29,18 +29,23 @@ class SkipList():
 	"""
 
 	MAX_LEVEL = 16
+	MAX_LEVEL_THRESHOLD = 2**MAX_LEVEL
+	head = property(fget=lambda self: self._head, doc="The head of list.")
 
 	def __init__(self, prototype=SkipListNode):
+		"Init."
 		self._prototype = prototype
 		self._head = self._prototype(None)
-		self.head = self._head
 
 	def generateLevel(self):
-		seed = R.randint(0, 2**self.MAX_LEVEL)
+		"Generates new level with fixed probability."
+		seed = 0
 		level = 0
 		found = 0
 
 		while not found:
+			if (seed < 2):
+				seed = R.randint(0, self.MAX_LEVEL_THRESHOLD)
 			level += 1
 			found = seed % 2
 			seed /= 2
@@ -51,6 +56,11 @@ class SkipList():
 		return level
 
 	def search(self, value, forInsert=False):
+		"""
+		Search for node with given key, if forInsert is given
+		return list of nodes at each level after which new node
+		should be placed.
+		"""
 		node = self._head
 
 		if forInsert:
@@ -72,10 +82,13 @@ class SkipList():
 
 		if forInsert:
 			return result
-		else:
+		elif node.key == value:
 			return node
+		else:
+			return False
 
 	def addNode(self, node):
+		"Adds new node to list."
 		levels = self.generateLevel()
 		nodes = self.search(node.key, True)
 		for level in range(levels, 0, -1):
@@ -85,6 +98,20 @@ class SkipList():
 			except KeyError:
 				self._head.set(node, level)
 
-	def addValue(self, value):
+	def insert(self, value):
+		"Adds new value to list."
 		node = self._prototype(value)
 		return self.addNode(node)
+
+	def getValues(self, level=1):
+		"Returns list of nodes at specified level."
+		node = self._head
+		result = []
+		while True:
+			tmp = node.next(level)
+			if not tmp:
+				break
+			result.append(tmp.key)
+			node = tmp
+		return result
+
